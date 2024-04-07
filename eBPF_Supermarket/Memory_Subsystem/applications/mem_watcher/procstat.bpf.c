5573601ad4a2aced7ae844b99ceb04df9c64f417
syxl-time <953879556@qq.com>
2024-04-07 21:55:03 +0800

    修改流水线

--- eBPF_Supermarket/Memory_Subsystem/applications/mem_watcher/procstat.bpf.c
+++ eBPF_Supermarket/Memory_Subsystem/applications/mem_watcher/procstat.bpf.c
@@ -11,6 +11,7 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
+#include <linux/mm_types.h>
 #include "mem_watcher.h"
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
